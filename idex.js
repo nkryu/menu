3422f972544a94b28eb4274d60d28c603e1cfe6c
Kiryu Niikura <snk1222@icloud.com>
2021-01-31 00:57:22 +0900

    create

---
+++ idex.js
@@ -0,0 +1,22 @@
+const body = document.body;
+const btn = document.querySelectorAll('span');
+const menu = document.getElementById('js-line')
+const contents = document.getElementById('js-menu')
+const li = document.querySelectorAll('li')
+
+//イベント
+const clickEvent = ()=>{
+  for(let i = 0; i < btn.length; i++){
+    btn[i].classList.toggle(`line${i+1}`);
+    contents.classList.toggle("contents")
+    body.classList.toggle('scroll')
+  }
+}
+
+menu.addEventListener('click',foo);
+
+for(let i = 0 ; i < li.length ; i++){
+  li[i].addEventListener('click',foo);
+}
+
+
